401c7a12b580ee8f04fedb4dfad88d1306292985
Dmytro Cisar <dmytro.cisar@gmail.com>
2017-10-15 18:54:13 +0300

    Minimal working example

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const gulpAtomizer = require('gulp-atomizer');
+const gulpHtmlMin = require('gulp-html-minifier');
+const browserSync = require('browser-sync').create();
+
+gulp.task('bs', ['html', 'acss'], () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+
+  gulp.watch(["src/*.html"], ['html', 'acss']);
+});
+
+gulp.task('html', function() {
+  return gulp.src(['src/*.html'])
+    .pipe(gulpHtmlMin({
+      collapseInlineTagWhitespace: true,
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('acss', () => {
+  return gulp.src('src/*.html')
+    .pipe(gulpAtomizer({
+      outfile: 'atomic.css'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('rebuild', ['html', 'acss']);
+
+gulp.task('default', ['rebuild', 'bs']);
