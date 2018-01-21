7f90e4b3441d1722a3d8ca33d348f16c9db56904
Dmytro Cisar <i@dmyt.ro>
2018-01-21 10:34:50 +0200

    + rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const gulpAtomizer = require('gulp-atomizer');
 const browserSync = require('browser-sync').create();
+const emmetRules = require('atomizer-emmet-compat');
 
 gulp.task('bs', ['acss'], () => {
   browserSync.init({
@@ -21,8 +22,8 @@ gulp.task('acss', () => {
   return gulp.src('*.html')
     .pipe(gulpAtomizer({
       outfile: 'atomic.css',
-      acssConfig: require('./config.js'),
-      addRules: require('atomizer-emmet-compat')
+      acssConfig: require('./config'),
+      addRules: require('./rules').concat(emmetRules)
     }))
     .pipe(gulp.dest('dist'));
 });
---
+++ rules.js
@@ -0,0 +1,5 @@
+// Example: https://github.com/acss-io/gulp-atomizer/blob/master/test/rules.js
+
+module.exports = [
+
+];
